d278789e557a55d324700f65de7b153024eea2f9
Will Vokins <will.vokins@outlook.com>
2021-02-05 23:26:28 +0000

    Second Commit

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from flask_pymongo import PyMongo
+
+import db
+import forms
+
 app = Flask(__name__)
-app.config["version"] = "Alpha 1"
+app.config['version'] = 'Alpha 1'
+app.config['SECRET_KEY'] = '06428f29a279cddf7fac4b0180db9579'
 
 @app.route('/')
 def root():
@@ -9,3 +15,7 @@ def root():
 @app.route('/login')
 def login():
     return render_template("login.html"), 200
+
+@app.route('/register')
+def register():
+    return render_template("register.html"), 200
