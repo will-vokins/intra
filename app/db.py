6cbfbd34226a252cc2766e68b886e95008397190
Will Vokins <78571349+will-vokins@users.noreply.github.com>
2021-10-24 20:39:45 +0100

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_pymongo import pymongo
 
-CONNECTION_STRING = "mongodb+srv://application:bd464443316fcbd3b07ce651e16358a4@cluster0.fzjuy.mongodb.net/intradb?retryWrites=true&w=majority"
+CONNECTION_STRING = "censored"
 
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('intradb')
